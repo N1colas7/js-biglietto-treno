4238abe1648a4fae93ea3a8ff9e46d4069b5ba60
N1colas7 <nicolasnd9897@gmail.com>
2022-11-25 17:28:27 +0100

    biglietto1

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ const eta = parseInt ( prompt('Quanti anni hai?'));
 console.log( eta );
 
 if ( 'eta' >= 18 && 'eta' <= 63 ) {
-   
+   const biglietto_1 =  km * 0.21   
 }else if ( 'eta' <= 17 ){
     
 }
+
+console.log( biglietto_1 );
