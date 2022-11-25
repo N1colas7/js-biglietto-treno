8925713f8cf154e692d76fceae0f3108d9fc052c
N1colas7 <nicolasnd9897@gmail.com>
2022-11-25 17:17:46 +0100

    skeleton

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const km = parseInt ( prompt('Quanti chilometri vuoi percorrere?'));
+console.log( km );
+const eta = parseInt ( prompt('Quanti anni hai?'));
+console.log( eta );
+
+if ( 'eta' >= 18 && 'eta' <= 63 ) {
+   
+}else if ( 'eta' <= 17 ){
+    
+}
